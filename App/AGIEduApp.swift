053f8c07f308d91e1d4d9f543742b9e13d41c8d1
Laurin Carmichael <ldc66@cornell.edu>
2025-09-30 20:45:40 -0700

    Create AGIEduApp.swift

---
+++ App/AGIEduApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+/// Entry point for the AGI.edu application.
+@main
+struct AGIEduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
